8eccb5a0067a791fef977054149224149fd0aff6
jd richards <jdrichardstech@gmail.com>
2023-12-06 16:25:32 -0500

    boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Aside from "./components/Aside";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+import recipes from "./data/data.json";
+
+const name = {
+  firstName: "Add a string value with your First Name",
+  lastName: "Add a string value with your Last Name"
+};
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Aside />
+        <Main />
+      </div>
+    </div>
+  );
 };
 
 export default App;
