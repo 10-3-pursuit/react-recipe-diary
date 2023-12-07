fca0229fb39fbf59219f1b2518e0bf544d11f6a7
KenCab123 <KennethCabral@pursuit.org>
2023-12-07 14:38:31 -0500

    Finished

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,33 @@
-const Aside = () => {
-  return <div>Aside</div>;
+const Aside = ({name, recipes}) => {
+  return (
+    <aside>
+    <h2>{name.firstName}'s Lists</h2>
+    <h3>Vegetarian</h3>
+    <ul>
+      {recipes.map(recipe => {
+        return <li key={recipe.title}>
+          {recipe.isVegetarian === true ? recipe.title: null}
+        </li>
+      })}
+    </ul>
+    <h3>Non-Vegetarian</h3>
+    <ul>
+      {recipes.map(recipe => {
+        return <li key={recipe.id}>
+          {recipe.isVegetarian === false ? recipe.title: null}
+        </li>
+      })}
+    </ul>
+    <h3>40 minutes or less</h3>
+    <ul>
+      {recipes.map(recipe => {
+        return <li key={recipe.id}>
+          {recipe.minutesToCook <= 40 ? recipe.title : null}
+        </li>
+      })}
+    </ul>
+    </aside>
+  )
 };
 
 export default Aside;
