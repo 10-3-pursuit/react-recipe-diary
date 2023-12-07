00a844a9022ede27e846a023de3100520ddf0079
brenda-soto <bsoto@pursuit.org>
2023-12-07 15:16:22 -0500

    React Recipe Diary lab complete

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,21 @@
-const Aside = () => {
-  return <div>Aside</div>;
+const Aside = ({name, recipes}) => {
+  return (
+    <aside>
+    <h2>{name.firstName}'s Lists:</h2>
+    <h3>Vegetarian</h3>
+    <ul>
+      {recipes.map(recipe => recipe.isVegetarian && <li key={recipe.id} >{recipe.title}</li>)}
+    </ul>
+    <h3>Non-Vegetarian</h3>
+    <ul>
+    {recipes.map(recipe => !recipe.isVegetarian && <li key={recipe.id} >{recipe.title}</li>)}
+    </ul>
+    <h3>40 Minutes or less</h3>
+    <ul>
+    {recipes.map(recipe => recipe.minutesToCook <= 40 && <li key={recipe.id} >{recipe.title}</li>)}
+    </ul>
+  </aside>
+  )
 };
 
 export default Aside;
