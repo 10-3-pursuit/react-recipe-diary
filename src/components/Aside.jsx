c24bad0393cfffc446afe5f46799fdb001723fdc
Jose Barrios <josebarrios@pursuit.org>
2023-12-07 15:06:45 -0500

    finished lab

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,16 @@
-const Aside = () => {
-  return <div>Aside</div>;
+const Aside = (props) => {
+  return <aside>
+    <h2>{props.name.firstName}'s Lists:</h2>
+    <h3>Vegetarian</h3>
+    <ul>{props.recipes.map((recipe) => recipe.isVegetarian === true && <li key={recipe.id}>{recipe.title}</li>)}</ul>
+
+    <h3>Non-Vegetarian</h3>
+    <ul>{props.recipes.map((recipe) => recipe.isVegetarian === false && <li key={recipe.id}>{recipe.title}</li>)}</ul>
+
+    <h3>40 Minutes or less</h3>
+    <ul>{props.recipes.map((recipe) => recipe.minutesToCook <= 40 && <li key={recipe.id}>{recipe.title}</li>)}</ul>
+    </aside>;
+    
 };
 
 export default Aside;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,16 @@
-const Main = () => {
-  return <div>Main</div>;
+const Main = (props) => {
+  return <main>
+    <h2>All Recipes</h2>
+    <ul>{props.recipes.map((recipe) =>
+      <li key={recipe.id}>
+        <h4>{recipe.title}</h4>
+        <p>{recipe.summary}</p>
+        <p>Recipe created by chef {recipe.chef}</p>
+        <p>Year Created: {recipe.yearCreated}</p>
+        <span>Preparation Time: {recipe.minutesToCook} minutes</span>
+      </li>)}
+    </ul>
+  </main>;
 };
 
 export default Main;
