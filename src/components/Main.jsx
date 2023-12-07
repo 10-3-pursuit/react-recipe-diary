242664afcae4af4dd5ac11332325aa6d02b5ef8d
Carlitos <carlitosdutan@pursuit.org>
2023-12-07 16:15:44 -0500

    Lab Completed +1 Bonus

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,18 @@
-const Main = () => {
-  return <div>Main</div>;
-};
+const Main = (props) => {
+  return( 
+  <main>
+    <h2>All Descriptions</h2>
+    {props.recipes.map((recipe) => (
+      <div key={recipe.id}>
+        <br />
+        <h4>{recipe.title}</h4>
+        <p>{recipe.summary}</p>
+        <span>{`Preparation Time: ${recipe.minutesToCook} minutes`}</span>
+        <br />
+      </div>
+    ))}
+
+  </main>
+)};
 
 export default Main;
