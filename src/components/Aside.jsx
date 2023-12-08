38882b252ca89c03e0ff01fa5dde99cf4708c279
juligarc91 <julissagarcia@pursuit.org>
2023-12-07 23:29:09 -0500

    Created vegetarian ul and li for Aside Component

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,10 @@
-const Aside = () => {
-  return <aside>Aside</aside>;
+const Aside = ({name, recipes}) => {
+  return <aside>
+    <h2>{name.firstName}'s Lists</h2>
+    <h3>Vegetarian</h3>
+    <ul>{recipes.map((recipe) => (recipe.isVegetarian ? <li key={recipes.isVegetarian}>{recipe.isVegetarian}</li> : null))}</ul>
+    {/* non vegetarian */}
+    <li></li>
+  </aside>;
 };
-
 export default Aside;
