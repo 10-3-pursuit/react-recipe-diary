3c6af3a5b8c910e0bc72f337534a22fe0ffc1c9a
enochtagoe <enochtagoe@pursuit.org>
2023-12-10 00:44:52 -0500

    push updates

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,11 +5,11 @@ const Aside = ({name, recipes}) => {
     <h3>Vegetarian</h3>
     <ul>
       <h3>
-        {recipes.map(recipe => recipe.isVegetarian ? <li key= {recipes.id}>{recipe.title}</li> : null)}
+        {recipes.map(recipe => recipe.isVegetarian ? <li key= {recipe.id}>{recipe.title}</li> : null)}
         <ul>
           <h3>Non-Vegetarian</h3>
           <ul>{recipes.map(recipe => 
-            !recipe.isVegetarian ? null : <li key= {recipe.id}>{recipe.title}</li>)}
+            !recipe.isVegetarian ?  <li key= {recipe.id}>{recipe.title}</li> : null)}
           </ul>
           <h3> 40 Minutes or Less</h3>
           <ul>{recipes.map(recipe => recipe.minutesToCook <= 40 && <li key={recipe.id}>{recipe.title}</li>)}</ul>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,8 @@ const Main = ({recipes}) => {
     <main>
       <h2>All Descriptions</h2>
       {recipes.map(recipe => recipe.title ? (
-        <ul>
+        <ul key={recipe.id} >
+          
           <h4>{recipe.title}</h4>
           <p>{recipe.summary}</p>
           <span>Preparation time: {recipe.minutesToCook} minutes</span>
