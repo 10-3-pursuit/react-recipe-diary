47a34d589009fda1c7b97261a529291a7b221a40
Lenny Lopez <lennylopez@pursuit.org>
2023-12-22 13:04:37 -0500

    recipe

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,18 @@
-const Aside = () => {
-  return <div>Aside</div>;
+const Aside = ({name, recipes}) => {
+  return <aside>
+
+  <h2>{name.firstName}'s Lists </h2> 
+    <h3>Vegetarian</h3>
+      <ul>
+        {recipes.map(recipe=> recipe.isVegetarian && <li key={recipe.id} >{recipe.title}</li>)}
+        </ul>
+      <h3>Non-Vegetarian</h3>
+      <ul>
+        {recipes.map(recipe =>!recipe.isVegetarian && <li key={recipe.id}>{recipe.title}</li>)}
+        <li></li>
+      </ul>
+
+    </aside>
 };
 
 export default Aside;
